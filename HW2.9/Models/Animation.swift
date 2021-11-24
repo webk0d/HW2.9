8fcc67097fe3fc4d9f31ccbb7cdcb1aa2e181151
SergeyChumakov <webk0d@ya.ru>
2021-11-24 11:57:06 +0300

    fix

--- HW2.9/Models/Animation.swift
+++ HW2.9/Models/Animation.swift
@@ -1,5 +1,5 @@
 //
-//  Animation.swift
+//  File.swift
 //  HW2.9
 //
 //  Created by Сергей Чумаков on 24.11.2021.
@@ -11,14 +11,14 @@ struct Animation {
     let curve: String
     let force: Float
     let duration: Float
-    let delay: Floa
+    let delay: Float
     
     static func getRandomAnimation() -> Animation {
         return Animation(
-            name: DataManager.shared.animations.randomElement()?.rawValue ?? "slideLeft",
-            curve: DataManager.shared.curves.randomElement()?.rawValue ?? "easeIn",
-            force: Float.random(in: 1...2),
-            duration: Float.random(in: 1...3),
+            name: DataManager.shared.animations.randomElement()?.rawValue ?? "shake",
+            curve: DataManager.shared.curves.randomElement()?.rawValue ?? "linear",
+            force: Float.random(in: 0.5...1.5),
+            duration: Float.random(in: 0.5...2.5),
             delay: Float.random(in: 0.1...0.5)
         )
     }
